43c4d28ccfb77bfffcd5427e3415371b5142b715
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-06-20 17:39:12 +0530

    Add files via upload

---
+++ 14_conditional_statements/if_else.s
@@ -0,0 +1,53 @@
+.section .rodata
+    msg_main_print_enter:
+    .string "Enter number : \t"
+
+    msg_main_scan:
+    .string "%d"
+
+    msg_main_print_even:
+    .string "Number is even\n"
+
+    msg_main_print_odd:
+    .string "Number is odd\n"
+
+.section .text
+.globl  main
+.type   main, @function
+main:
+    pushl   %ebp
+    movl    %esp, %ebp
+
+    subl    $4, %esp
+
+    pushl   $msg_main_print_enter
+    call    printf
+    addl    $4, %esp
+
+    leal    -4(%ebp), %ebx
+    pushl   %ebx
+    pushl   $msg_main_scan
+    call    scanf
+    addl    $4, %esp
+
+    movl    $2, %ecx
+    movl    -4(%ebp), %eax
+    xorl    %edx, %edx
+    divl    %ecx
+    cmpl    $0, %edx
+    jne     label_else
+    pushl   $msg_main_print_even
+    call    printf
+    addl    $4, %esp
+    jmp     label_exit
+
+label_else:
+    pushl   $msg_main_print_odd
+    call    printf
+    addl    $4, %esp
+
+label_exit:
+    pushl   $0
+    call    exit
+
+    
