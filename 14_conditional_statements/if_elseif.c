a72f84f7ef30af342eec263749519958c46b315a
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-06-20 17:44:43 +0530

    Add files via upload

---
+++ 14_conditional_statements/if_elseif.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int iNo;
+
+    printf("Enter number : \t");
+    scanf("%d", &iNo);
+
+    if (iNo < 2)
+        printf("Number is less than 2\n");
+    else if(iNo > 2)
+        printf("Number is greater than 2\n");
+    else
+        printf("Number is 2\n");
+
+    exit(0);
+}
