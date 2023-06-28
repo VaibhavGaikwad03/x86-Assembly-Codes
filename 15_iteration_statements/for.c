b1141bd45f121d3463b2fe24054b9d4a2a70302a
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-06-28 22:36:30 +0530

    Add files via upload

---
+++ 15_iteration_statements/for.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int iNo;
+    int iCounter;
+
+    printf("Enter a number  :\t");
+    scanf("%d", &iNo);
+
+    for (iCounter = 0; iCounter < iNo; iCounter++)
+        printf("%d\n", iCounter);
+
+    exit(0);
+}
