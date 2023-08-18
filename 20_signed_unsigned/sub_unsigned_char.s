a21e19817ff079c83302f977ea71fce0a3aa9d18
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-08-18 23:09:04 +0530

    Add files via upload

---
+++ 20_signed_unsigned/sub_unsigned_char.s
@@ -0,0 +1,34 @@
+.section .rodata
+    msg_main_print_1:
+    .string "Carry bit on\n"
+
+    msg_main_print_2:
+    .string "Carry bit off\n"
+
+.section .text
+.globl  main 
+.type   main, @function
+main:
+    andl    $-16, %esp
+    pushl   %ebp
+    movl    %esp, %ebp
+
+    subl    $16, %esp
+
+    movb    $3, %al
+    movb    $4, %dl
+    subb    %dl, %al
+
+    jc      label_carry_bit_on
+    movl    $msg_main_print_2, (%esp)
+    call    printf
+    jmp     label_exit
+
+label_carry_bit_on:
+    movl    $msg_main_print_1, (%esp)
+    call    printf
+
+label_exit:
+
+    movl    $0, (%esp)
+    call    exit
